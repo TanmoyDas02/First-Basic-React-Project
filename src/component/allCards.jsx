04ad0a62fcd9dae10e0ab7d78b9953da45918af4
Tanmoy Das <tanmoy021997@gmail.com>
2024-06-07 23:57:48 +0530

    React first basic project using Tailwind

--- src/component/allCards.jsx
+++ src/component/allCards.jsx
@@ -1,19 +1,28 @@
-
-function AllCards(){
+function AllCards({ data }) {
     return (
-        <div style={{display:"flex", flexWrap:"wrap", gap:"20px"}}>
+        <div className="mt-10 flex flex-wrap justify-around w-[80%] gap-10 mb-10">
+            {data.map((singleItem) => (
+                <div
+                    key={singleItem.id}
+                    className="bg-red-400 w-[25%] max-h-[500px] p-4 rounded-xl shadow-xl"
+                >
+                    <img
+                        src={singleItem.thumbnail}
+                        className="aspect-square w-[100%] object-cover h-[70%] rounded-xl shadow-xl"
+                    />
+                    <div className="flex items-center justify-between">
+                        <h2 className="text-left my-5 text-xl line-clamp-1">
+                            {" "}
+                            {singleItem.title}
+                        </h2>
+                        <p className="text-4xl">${singleItem.price}</p>
+                    </div>
 
-            {data.map(singleItem => (
-                <div key={singleItem.id} style={{backgroundColor:"purple", width:"250px", height:"300px", overflow:"hidden"}}>
-                <img src={singleItem.thumbnail} style={{width:"60%", objectFit:"cover"}} />
-                <h2>{singleItem.title}</h2>
-                <p>{singleItem.price}$</p>
-                <p>{singleItem.description}</p>
-            </div>
+                    <p className="text-left line-clamp-1 lg:line-clamp-2">{singleItem.description}</p>
+                </div>
             ))}
-            
         </div>
-    )
+    );
 }
 
 export default AllCards;
